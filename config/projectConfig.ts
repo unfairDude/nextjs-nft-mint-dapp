d63bb0d43a1ec9c6b8f1f409e9f3fe0a4ab030e1
unfairDude <easynafta@gmail.com>
2022-05-06 19:50:10 +0200

    price fix

--- config/projectConfig.ts
+++ config/projectConfig.ts
@@ -7,7 +7,7 @@ const projectConfig = {
 
   maxMintAmountPerTxn: 10,
 
-  mintCost: process.env.NODE_ENV === 'production' ? 0.002129 : 0.002129,
+  mintCost: process.env.NODE_ENV === 'production' ? 5.5 : 5.5,
 
   networkName:
     process.env.NODE_ENV === 'production'
