5059b55b17b8b824814a3a5b724e393d1ae5c220
unfairDude <easynafta@gmail.com>
2022-05-07 03:23:15 +0200

    vcvcv

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,12 +1,13 @@
 export default function Faq() {
   return (
     <>
-      <h2 className="text-4xl mb-4">Mint & Win</h2>
+      <h2 className="text-4xl mb-4">FAQ</h2>
       <div className="space-y-4">
         <div>
           <h3 className="text-2xl mb-2">How much is the minting price?</h3>
           <p>
           hello
+          </p>
           
 
         </div>
