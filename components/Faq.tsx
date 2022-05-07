59e0b50934130f49336769949da7b6aea292961b
unfairDude <easynafta@gmail.com>
2022-05-07 14:27:40 +0200

    gdgdfgfg

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,11 +1,11 @@
 export default function Faq() {
   return (
     <>
-      <h2 className="text-5xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉 𝙋𝙍𝙊𝙅𝙀𝘾𝙏</h2>
+      <h2 className="text-5xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉</h2>
       <div className="space-y-4">
         <div>
-          <h3 className="text-3xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆?</h3>
-          <p className="text-xl">
+          <h3 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆?</h3>
+          <p className="text-1xl">
             𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 𝘢𝘯 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏,<br></br>
             𝘵𝘩𝘦𝘳𝘦&#39;𝘴 𝘢 𝘨𝘰𝘰𝘥 𝘤𝘩𝘢𝘯𝘤𝘦 𝘵𝘩𝘦𝘺&#39;𝘭𝘭 𝘨𝘦𝘵 𝘴𝘰𝘮𝘦 𝙧𝙖𝙧𝙚 𝙩𝙧𝙖𝙞𝙩𝙨,<br></br>
             𝘸𝘩𝘪𝘤𝘩 𝘣𝘢𝘴𝘦𝘥 𝘰𝘯 𝘳𝘢𝘳𝘪𝘵𝘺, 𝘸𝘪𝘭𝘭 𝘣𝘦 𝙧𝙚𝙬𝙖𝙧𝙙𝙚𝙙 𝙬𝙞𝙩𝙝 𝙪𝙥 𝙩𝙤 𝟓𝟎𝟎$𝙈𝘼𝙏𝙄𝘾<br></br>
