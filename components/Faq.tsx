afea85835383820b15f347a6fbc5f7aa907d8e64
unfairDude <easynafta@gmail.com>
2022-05-07 13:48:47 +0200

    vgvgvgv

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -2,16 +2,14 @@ export default function Faq() {
   return (
     <>
       <h2 className="text-5xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉 𝙋𝙍𝙊𝙅𝙀𝘾𝙏</h2>
-      <div className="bg-gray-800 py-8">
-        <div>
+      <div className="space-y-4">
+        <Prose>
           <h3 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆？</h3>
           <p className="text-xl">
           𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 𝘢𝘯 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏,<br></br>
           𝘵𝘩𝘦𝘳𝘦'𝘴 𝘢 𝘨𝘰𝘰𝘥 𝘤𝘩𝘢𝘯𝘤𝘦 𝘵𝘩𝘦𝘺'𝘭𝘭 𝘨𝘦𝘵 𝘴𝘰𝘮𝘦 𝙧𝙖𝙧𝙚 𝙩𝙧𝙖𝙞𝙩𝙨, 
           </p>
-          
-
-        </div>
+        <Prose>
       </div>
     </>
   );
