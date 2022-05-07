75a0a05760875ad7c69c1b62269fd0f274a04cda
unfairDude <easynafta@gmail.com>
2022-05-07 13:28:31 +0200

    gdgdgdg

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -6,7 +6,7 @@ export default function Faq() {
         <div>
           <h3 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆？</h3>
           <p>
-          𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 
+          𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 𝘢𝘯 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏,<br></br>
           </p>
           
 
