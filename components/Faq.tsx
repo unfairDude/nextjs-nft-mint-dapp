e19545c910501f29bb2f736c7b7afb00fead7101
unfairDude <easynafta@gmail.com>
2022-05-07 14:18:13 +0200

    gdgdgg

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -7,7 +7,7 @@ export default function Faq() {
           <h3 className="text-2xl mb-2">How much is the minting price?</h3>
           <p>
             𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 𝘢𝘯 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏,
-            𝘵𝘩𝘦𝘳𝘦 𝘪𝘴 𝘢 𝘨𝘰𝘰𝘥 𝘤𝘩𝘢𝘯𝘤𝘦 𝘥𝘰𝘯𝘵 ut labore et dolore magna aliqua. Ut enim
+            𝘵𝘩𝘦𝘳𝘦 𝘪𝘴 𝘢 𝘨𝘰𝘰𝘥 𝘤𝘩𝘢𝘯𝘤𝘦 𝘥𝘰𝘯&#39;𝘵 ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
