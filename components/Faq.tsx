70b1619637baa2918eeca802575edf28bd94e6bc
unfairDude <easynafta@gmail.com>
2022-05-07 13:38:02 +0200

    bhbhbhbhbhbhbh

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -4,8 +4,8 @@ export default function Faq() {
       <h2 className="text-5xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉 𝙋𝙍𝙊𝙅𝙀𝘾𝙏</h2>
       <div className="space-y-4">
         <div>
-          <h1 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆？</h1>
-          <p>
+          <h3 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆？</h3>
+          <p className="text-xl">
           𝘞𝘩𝘦𝘯𝘦𝘷𝘦𝘳 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘮𝘪𝘯𝘵 𝘢𝘯 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏,<br></br>
           </p>
           
