5f8729f7553e0957129b113d57b0e8350261ba07
unfairDude <easynafta@gmail.com>
2022-05-07 13:22:45 +0200

    bhbhbh

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,7 +1,7 @@
 export default function Faq() {
   return (
     <>
-      <h2 className="text-4xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉 𝙋𝙍𝙊𝙅𝙀𝘾𝙏</h2>
+      <h2 className="text-5xl mb-4">𝙈𝙄𝙉𝙏 & 𝙒𝙄𝙉 𝙋𝙍𝙊𝙅𝙀𝘾𝙏</h2>
       <div className="space-y-4">
         <div>
           <h3 className="text-2xl mb-2">𝙃𝙊𝙒 𝘿𝙊𝙀𝙎 𝙄𝙏 𝙒𝙊𝙍𝙆？</h3>
