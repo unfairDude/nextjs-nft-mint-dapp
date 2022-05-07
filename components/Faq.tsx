c82772cfe5babc6220a04efb8ad060b22bcc40a0
unfairDude <easynafta@gmail.com>
2022-05-07 16:52:21 +0200

    bhbhbhbhb

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -15,10 +15,7 @@ export default function Faq() {
             𝘈𝘯𝘥 𝘪𝘧 𝘺𝘰𝘶 𝘥𝘰𝘯&#39;𝘵 𝘸𝘪𝘯?<br></br>
             𝘞𝘦𝘭𝘭, 𝘺𝘰𝘶 𝘴𝘵𝘪𝘭𝘭 𝘩𝘢𝘷𝘦 𝘢𝘯 𝘶𝘯𝘪𝘲𝘶𝘦 𝘼𝙡𝙞𝙚𝙣𝙨-𝙀𝙑𝙊 𝙉𝙁𝙏
           </p>
-          <a href="prizes.html"<button class="wallet-btn btn">
-          <span>Rewards and Lucky Traits</span>
-        </button>
-        </a>
+          <button>Like</button>
         </div>
       </div>
     </>
